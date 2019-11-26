5aeef4c1d92734f6f9f6d45da6c5a3c94fc7682f
aleksa <sukovic.aleksa@gmail.com>
2019-11-26 17:21:04 +0100

    Adds builder pattern

--- 5/index.php
+++ 5/index.php
@@ -4,9 +4,12 @@ require_once 'Singleton/Singleton.php';
 require_once 'Monade/GeneratesRandomNumber.php';
 require_once 'Monade/NumberGenerator.php';
 require_once 'Monade/IntNumberGenerator.php';
+require_once 'Builder/User.php';
+require_once 'Builder/UserBuilder.php';
 
 use Amplitudo\Patterns\Singleton\Singleton;
 use Amplitudo\Patterns\Monade\NumberGenerator;
+use Amplitudo\Patterns\Builder\UserBuilder;
 
 //$instance = new Singleton(); // private constructor
 $instance1 = Singleton::getInstance();
@@ -24,6 +27,30 @@ $instance3 = new NumberGenerator();
 echo '<h1>' . $instance1->getID() . '</h1>';
 echo '<h1>' . $instance2->getID() . '</h1>';
 echo '<h1>' . $instance3->getID() . '</h1>';
+
+$user = (new UserBuilder())
+    ->male()
+    ->setFirstName('Marko')
+    ->setLastName('Markovic')
+    ->wage(250)
+    ->setEyeColor('blue')
+    ->setHairColor('brow')
+    ->build();
+
+echo '<h1>' . $user->toString() . '</h1>';
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
